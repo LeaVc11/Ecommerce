10da6ac0107ba7c00d44537576b6d085c541d667
Carine V <77583125+LeaVc11@users.noreply.github.com>
2024-06-01 13:55:10 +0200

    TestUnit

--- tests/UserUnitTest.php
+++ tests/UserUnitTest.php
@@ -15,6 +15,37 @@ class UserUnitTest extends TestCase
              ->setFirstName('prenom')
              ->setLastName('nom')
              ->setPassword('password');
-        $this->assertTrue(true);
+
+        $this->assertTrue($user->getEmail() ==='true@test.com');
+        $this->assertTrue($user->getFirstname() ==='prenom');
+        $this->assertTrue($user->getLastname() ==='nom');
+        $this->assertTrue($user->getPassword() ==='password');
+
+    }
+    public function testIsFalse(): void
+    {
+        $user = new User();
+
+        $user->setEmail('true@test.com')
+            ->setFirstName('prenom')
+            ->setLastName('nom')
+            ->setPassword('password');
+
+        $this->assertFalse($user->getEmail() ==='false@test.com');
+        $this->assertFalse($user->getFirstname() ==='false');
+        $this->assertFalse($user->getLastname() ==='false');
+        $this->assertFalse($user->getPassword() ==='false');
+
+    }
+    public function testIsEmpty(): void
+    {
+        $user = new User();
+
+        $this->assertEmpty($user->getEmail());
+        $this->assertEmpty($user->getFirstname());
+        $this->assertEmpty($user->getLastname());
+        $this->assertEmpty($user->getPassword());
+
+
     }
 }
