7e7acfb3e95e4bd20e19f7e1ce8915a18c50302e
Carine V <77583125+LeaVc11@users.noreply.github.com>
2024-06-02 16:25:26 +0200

    Ajout Fixtures user

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User();
+
+            $user->setEmail($faker->email())
+                ->setFirstname($faker->firstName())
+                ->setLastname($faker->lastName());
+
+            $user->setPassword($this->hasher->hashPassword($user, "user"));
+
+            $manager->persist($user);
+
+            $manager->flush();
+
+        }
+    }
+}
