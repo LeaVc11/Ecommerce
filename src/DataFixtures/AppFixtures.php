439c0a2bab0e8404f0437ee828d62cd3f7a53090
Carine V <77583125+LeaVc11@users.noreply.github.com>
2024-06-08 08:45:42 +0200

    Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,25 +15,23 @@ class AppFixtures extends Fixture
     public function __construct(UserPasswordHasherInterface $hasher)
     {
         $this->hasher = $hasher;
-
     }
 
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        for ($i = 0; $i < 5; $i++) {
+        for ($i = 0; $i < 5; ++$i) {
             $user = new User();
 
             $user->setEmail($faker->email())
                 ->setFirstname($faker->firstName())
                 ->setLastname($faker->lastName());
 
-            $user->setPassword($this->hasher->hashPassword($user, "user"));
+            $user->setPassword($this->hasher->hashPassword($user, 'user'));
 
             $manager->persist($user);
 
             $manager->flush();
-
         }
     }
 }
